66b8e69432d63aa4a5eceb8bc827929ca3599915
Ali Ibrahim <allosheribraheem38@gmail.com>
2021-08-11 15:45:20 +0300

    Fix time zone and site language

--- players/admin.py
+++ players/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Player
 
-# Register your models here.
+admin.site.site_header = 'Verwaltung'
+admin.site.site_title = 'Verwaltung'
+admin.site.index_title = 'Verwaltung'
+
+
+@admin.register(Player)
+class PersonAdmin(admin.ModelAdmin):
+    pass
--- web_application/settings.py
+++ web_application/settings.py
@@ -101,15 +101,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'de'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Berlin'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
