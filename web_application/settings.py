57c06af074b319ec500c62431ef02c71f943c379
Ali Ibrahim <allosheribraheem38@gmail.com>
2021-08-11 12:21:06 +0300

    Fix merger conflict

--- web_application/settings.py
+++ web_application/settings.py
@@ -124,5 +124,5 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-LOGIN_REDIRECT_URL = 'player/'
+LOGIN_REDIRECT_URL = '/player/'
 LOGIN_URL = '/login/'
