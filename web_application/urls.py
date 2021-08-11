1c5e765153e4e47f14562b66049d0c17c6a20f0a
Ali Ibrahim <allosheribraheem38@gmail.com>
2021-08-11 09:36:30 +0300

    Refactoring html pages

--- web_application/urls.py
+++ web_application/urls.py
@@ -17,8 +17,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
     path('player/', include('players.urls'))
 ]
