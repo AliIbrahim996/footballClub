76a0cbbba78d35806ce52076bf3d3b908545c9fc
Ali Ibrahim <allosheribraheem38@gmail.com>
2021-08-19 11:47:41 +0300

    add cutome filter to filter player skills result

--- players/templatetags/player_tags.py
+++ players/templatetags/player_tags.py
@@ -6,3 +6,8 @@ register = template.Library()
 @register.simple_tag
 def update_value(arg):
     return arg
+
+
+@register.filter
+def player_filter(player_skills, p_id):
+    return player_skills.filter(player=p_id)
